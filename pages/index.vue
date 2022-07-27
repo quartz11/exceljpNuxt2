ef6d18893f8cb9f0c39fa2662cf9b116dc34e105
KRITSAKORN CHOATCHAOJARU <amajakai14@gmail.com>
2022-07-27 15:46:33 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Navbar />
+    <Dropdown />
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+import Dropdown from "../components/Dropdown.vue";
+export default {
+  name: "IndexPage",
+  components: { Navbar, Dropdown },
+};
+</script>
