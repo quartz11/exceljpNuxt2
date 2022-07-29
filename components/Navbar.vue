c88e1ab7da9eec5e3f521318bd280d65901d9043
Rinku Inukowa <inukowarinku@gmail.com>
2022-07-29 13:07:44 +0700

    change navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,59 +1,73 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" class="navbar navbar-dark bg-primary" variant="info">
-      <b-navbar-brand href="#">Excellence Inc. Ltd.</b-navbar-brand>
+      <b-container>
+        <b-navbar-brand href="#">Excellence Inc. Ltd.</b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
 
-        <b-navbar-nav>
-          <b-nav-item href="/">Home</b-nav-item>
-          <b-nav-item href="/">Business</b-nav-item>
-          <b-nav-item href="/">Development</b-nav-item>
-          <b-nav-item href="/">Overview</b-nav-item>
-          <b-nav-item href="/">News</b-nav-item>
-          <b-nav-item href="/">Philosophy and Motto</b-nav-item>
-          <b-nav-item href="/">Contact</b-nav-item>
-          <b-nav-item href="/">Greeting</b-nav-item>
-          <b-nav-item href="/">Employment</b-nav-item>
-          <b-nav-item href="/">Access</b-nav-item>
+              <b-navbar-nav>
+                <b-nav-item-dropdown text=Home>
+                  <b-dropdown-item href="/">News</b-dropdown-item>
+                </b-nav-item-dropdown>
 
-        </b-navbar-nav>
+                <b-nav-item-dropdown text=Overview>
+                  <b-dropdown-item href="/">Company Overview</b-dropdown-item>
+                  <b-dropdown-item href="/">Our History</b-dropdown-item>
+                  <b-dropdown-item href="/">Our Philosophy</b-dropdown-item>
+                  <b-dropdown-item href="/">Greeting</b-dropdown-item>
+                </b-nav-item-dropdown>
 
-          </b-nav-form>
+                <b-nav-item-dropdown text=Business>
+                  <b-dropdown-item href="/">Our Business</b-dropdown-item>
+                  <b-dropdown-item href="/">Our Product</b-dropdown-item>
+                </b-nav-item-dropdown>
 
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
+                <b-nav-item href="/">Join us</b-nav-item>
 
-          <b-nav-item-dropdown right>
+                <b-nav-item href="/">Contact us</b-nav-item>
+
+              </b-navbar-nav>
+
+            </b-nav-form>
+
+            <b-nav-item-dropdown :text="lang" class=" rounded" right>
+              <b-dropdown-item @click="() => this.lang = 'JP'">JP</b-dropdown-item>
+              <b-dropdown-item @click="() => this.lang = 'EN'">EN</b-dropdown-item>
+              <b-dropdown-item @click="() => this.lang = 'TH'">TH</b-dropdown-item>
+              <b-dropdown-item @click="() => this.lang = 'CH'">CH</b-dropdown-item>
+            </b-nav-item-dropdown>
+
             <!-- Using 'button-content' slot -->
+            <!-- 
+          <b-nav-item-dropdownav-item-dropdown right>
             <template #button-content>
               <em>User</em>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
             <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
+          </b-nav-item-dropdownav-item-dropdown> 
+          -->
+
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
     </b-navbar>
 
     <br />
 
     <div class="d-flex flex-row-reverse bd-highlight">
-            <b-col cols="1">
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-col>
-            <b-col cols="2">
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            </b-col>
+      <b-col cols="1">
+        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+      </b-col>
+      <b-col cols="2">
+        <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+      </b-col>
     </div>
 
   </div>
@@ -62,5 +76,16 @@
 <script>
 export default {
   name: "Navbar",
+
+  data() {
+    return {
+      lang: 'EN'
+    }
+  },
+
+  methods: {
+
+  }
+
 };
 </script>
