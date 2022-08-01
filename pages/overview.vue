5227e98fa17ddd749b55d8bb692ac0bbe20cdaf7
Quart11 <sitthichot.01sp@gmail.com>
2022-08-01 16:54:09 +0700

    ADD Overview

---
+++ pages/overview.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+
+    <div class="container mx-auto">
+
+      <h1 class="text-lg text-center text-white bg-violet-800 ">Company Overview</h1>
+      <div class="relative overflow-x-auto shadow-md ">
+        <table class="w-full text-sm text-left text-white dark:text-gray-400">
+          <thead class="text-xs text-white uppercase dark:text-gray-400">
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="col" class="px-6 py-3 text-white dark:bg-violet-600 auto-cols-auto">
+                Company Name
+              </th>
+              <th scope="col" class="px-6 py-3 text-black bg-blue-300">
+                Excellence Inc. Ltd.
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Location
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                2F Animart-Nishiki 311 Uradeyama-cho KarasumaNishiIru Nishikikogi-Tohri Nakagyo-ku Kyoto-city Kyoto
+                604-8115 Japan
+              </td>
+
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Establishment
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                1989'12 20
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Capital
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                10,000,000
+              </td>
+            </tr>
+
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                President
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Atsunori Tokumasu
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Number of Employees
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                24 (2021'3 data)
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Member Organizations
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Kyoto Venture Business Club
+                <br />
+                Kyoto chamber of commerce Kyoto
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Bank
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Kyoto-Chuo Credit Union
+                <br />
+                Kyoto Bank
+                <br />
+                Kyoto Credit Union
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Business Registration
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                派２６－３００５３２ 一般労働者派遣事業者免許(Japan)
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+
+    </div>
+
+  </div>
+</template>
