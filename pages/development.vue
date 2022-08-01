46412c865a2bdaba354595ebb5d273f9ee15cee4
Quart11 <sitthichot.01sp@gmail.com>
2022-08-01 15:40:20 +0700

    ADD Development

---
+++ pages/development.vue
@@ -0,0 +1,79 @@
+<template>
+
+  <div>
+
+    <div class="container mx-auto">
+
+      <div class="bg-slate-50 card lg:card-side card-bordered">
+        <figure>
+          <img width="350" height="350" src="http://1excellence.com/official/img/deployment/medical.jpg">
+        </figure>
+        <div class="card-body">
+          <h2 class="text-black card-title">Treatment support application</h2>
+          <p class="text-black">It is a tablet application that was developed to focus on improvement realize improvement of the patient.
+Available in such as acupuncture salon, chiropractic bodywork.
+Photos, videos, body image and saved for each patient, it is possible to edit and comparison browsing.</p>
+          <p class="text-black">Introduction：</p>
+          <a href="http://1excellence.com/#Treatment%20support%20application">
+            <p class="text-sky-600">You can experience for one minute videos.</p>
+          </a>
+
+        </div>
+      </div>
+      <br />
+
+      <div class="bg-slate-50 card lg:card-side card-bordered">
+        <figure>
+          <img width="350" height="350" src="http://1excellence.com/official/img/deployment/warehouse.jpg">
+        </figure>
+        <div class="card-body">
+          <h2 class="text-black card-title">Bonded Warehouse System</h2>
+          <p class="text-black">It automates all of the detailed inventory management and work together.
+Fixed work what is the specialty of the machine.
+By your leave the risks associated with tied and data management to the complicated office work, let's aim to reduce labor costs.</p>
+          <p class="text-black">Introduction：</p>
+          <a href="http://1excellence.com/#Bonded%20Warehouse%20System">
+            <p class="text-sky-600">Is it possible to automate any work?</p>
+          </a>
+        </div>
+      </div>
+      <br />
+
+      <div class="bg-slate-50 card lg:card-side card-bordered">
+        <figure>
+          <img width="350" height="350" src="http://1excellence.com/official/img/deployment/groupware.jpg">
+        </figure>
+        <div class="card-body">
+          <h2 class="text-black card-title">Groupware System : e-base</h2>
+          <p class="text-black">Do you not Need help to the improvement of communication and solidarity between the on-premises?
+Daily, weekly, monthly, general affairs management, sales reporting, ranging from management's side feedback, can be done with a single login.</p>
+          <p class="text-black">Introduction：</p>
+          <a href="http://1excellence.com/#Groupware%20System%20:%20e-base">
+            <p class="text-sky-600">Currently, it is the system of the popular operating in our company.</p>
+          </a>
+        </div>
+      </div>
+      <br />
+
+      <div class="bg-slate-50 card lg:card-side card-bordered">
+        <figure>
+          <img width="350" height="350" src="http://1excellence.com/official/img/deployment/hotelReservation.jpg">
+        </figure>
+        <div class="card-body">
+          <h2 class="text-black card-title">Hotel Reservation System</h2>
+          <p class="text-black">
+You can easily search for bookable room.
+Room type and conditions also can be specified in any.</p>
+          <p class="text-black">Introduction：</p>
+          <a href="http://1excellence.com/#Hotel%20Reservation%20System">
+            <p class="text-sky-600">Any system?</p>
+          </a>
+        </div>
+      </div>
+      <br />
+
+    </div>
+
+  </div>
+
+</template>
