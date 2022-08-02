32f5bb368a9830e4de91db5df9009260bcfd29c5
Quart11 <sitthichot.01sp@gmail.com>
2022-08-02 14:12:21 +0700

    Add philosophy and Motto

---
+++ pages/motto.vue
@@ -0,0 +1,115 @@
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
+<template>
+  <section class="px-20">
+    <div class="container">
+
+      <h1 class="text-3xl text-black ">Corporate Philosophy</h1>
+      <br />
+      <!-- CUSTOM BLOCKQUOTE -->
+      <div class="p-4 text-gray-800 bg-white rounded-lg shadow-2xl max-w-8xl">
+        <div class="mb-2">
+          <table class="justify-center w-full table-auto ">
+            <thead>
+              <tr>
+                <th class="w-full "></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="content-center text-xl text-center">The pursuit of happiness of discretion both sides of all employees</td>
+              </tr>
+              <br />
+              <tr>
+                <td class="text-center">All employees of the company, management also including one technician, there spiritual of all the people that the gathering work, it is the pursuit of material happiness.
+
+That is, of happiness and is physically and spiritually both sides that we aim, and that through self-fulfillment in the field of work as well as go in search of economic stability and prosperity, and will seek the richness of the heart as a human being, such as life worth living and rewardingIt is.</td>
+              </tr>
+              <br />
+
+              <tr>
+                <td class="text-xl text-center">It will contribute to the realization of a livable in rich society</td>
+              </tr>
+              <br />
+
+              <tr>
+                <td class="text-center">Customers and the community, including to global environment steady around us worked hard in our efforts, that is has been put is the wish of the I want to become better by the realization of corporate activities.
+
+In addition, we polish always technology, by sending the world one after another excellent software, and continue to raise the profit as a company as well as contribute to the advancement of science and technology, and to contribute to the public welfare enhancement through such that you pay a lot of taxes to go.</td>
+              </tr>
+              <br />
+
+              <tr>
+                <td class="text-xl text-center">Heart of altruistic</td>
+              </tr>
+              <br />
+
+              <tr>
+                <td class="text-center">Idea that altruistic that in better than your own has become a backbone. Themselves even better by happy others, is the way to become happy.
+
+Only the spirit of the altruistic is not more to allow for sustainable development, it is that is to act in order to achieve those ideals is the purpose of our company.</td>
+              </tr>
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+      <br />
+      <!-- END -->
+
+      <h1 class="text-3xl text-black ">Company Motto</h1>
+      <br />
+      <!-- CUSTOM BLOCKQUOTE -->
+      <div class="p-4 text-gray-800 bg-white rounded-lg shadow-2xl max-w-8xl">
+        <div class="mb-2">
+          <table class="justify-center w-full table-auto ">
+            <thead>
+              <tr>
+                <th class="w-full "></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="content-center text-2xl text-center">敬天愛人</td>
+              </tr>
+              <br />
+              <tr>
+                <td class="text-xl text-center">「南洲翁遺訓」</td>
+              </tr>
+              <br />
+
+              <tr>
+                <td class="text-center">道というのはこの天地のおのずからなるものであり、人はこれにのっとって行うべきもの
+であるから何よりもまず、天を敬うことを目的とすべきである。
+<br />
+天は他人も自分も平等に愛したもうから、自分を愛する心をもって人を愛することが肝要である。</td>
+              </tr>
+              <br />
+
+              <tr>
+                <td class="text-center">To put contemporary say.
+the company or team is like a family.
+It is to little put up with selfishness among the organizations.</td>
+              </tr>
+              <br />
+
+              <tr>
+                <td class="text-center">It is very difficult to increase the heart but by Strive to realize in all employees,
+and I think it will lead to being able to embody the spiritual richness.</td>
+              </tr>
+              <br />
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+      <br />
+      <!-- END -->
+
+
+    </div>
+  </section>
+</template>
