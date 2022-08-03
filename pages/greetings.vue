c532f788d7e5c87f2a3b670c947b899ae3062383
Quart11 <sitthichot.01sp@gmail.com>
2022-08-03 17:59:07 +0700

    Add greetings

--- pages/greetings.vue
+++ pages/greetings.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
 
+    <div class="flex justify-center w-full mx-auto">
+      <div class="justify-center max-w-sm overflow-hidden rounded shadow-2xl bg-slate-50" style="max-width: 60rem;">
+        <div class="card-header" style="background-color:blueviolet; color:white;">&nbsp;&nbsp; Greetings</div>
+        <div class="card-body">
+          <table>
+            <thead>
+              <th class="text-black w-80">
+          <img width="350" height="350" src="http://1excellence.com/official/img/common/boss.jpg">
+          <br />
+          Atsunori Tokumasu
+          <br />
+Company President
+              </th>
+              <th class="w-full text-black">
+                <p>
+                  We at Excellence in December 1989
+In addition to the pursuit of happiness discretion both sides of all employees
+To provide the best of ICT services to customers
+It will contribute to the realization of a livable peace society.
+It was established under the management philosophy
+                </p>
+              </th>
+            </thead>
+          </table>
+        </div>
+      </div>
+    </div>
+    <br />
   </div>
 </template>
