aaf03644086ef9e72f84ce00f95cdda8f97c388e
Quart11 <sitthichot.01sp@gmail.com>
2022-08-04 16:26:47 +0700

    Edit format policy

--- pages/policy.vue
+++ pages/policy.vue
@@ -19,7 +19,11 @@
               </tr>
               <br />
               <tr>
-                <td class="text-xl text-center">Excellence Inc. Ltd. (hereinafter referred to as the Company) will understand that it is the assets personal information is important in advanced information communication society of today, able to handle personal information properly and is recognized as one of our important responsibility , we will declare that strive to protect personal information based on the following policy.</td>
+                <td class="text-xl text-center">Excellence Inc. Ltd. (hereinafter referred to as the Company) will
+                  understand that it is the assets personal information is important in advanced information
+                  communication society of today, able to handle personal information properly and is recognized as one
+                  of our important responsibility , we will declare that strive to protect personal information based on
+                  the following policy.</td>
               </tr>
             </tbody>
           </table>
@@ -27,14 +31,15 @@
         </div>
       </div>
 
-  </div>
-  <br />
-<br />
-<!-- Another box -->
+    </div>
+    <br />
+    <br />
+    <!-- Another box -->
 
     <div class="flex justify-center mx-auto">
       <div class="justify-center overflow-hidden rounded shadow-2xl bg-slate-50 w-11/12">
-        <div class="card-header text-xl" style="background-color:blueviolet; color:white;">&nbsp;&nbsp; Privacy Policy</div>
+        <div class="card-header text-xl" style="background-color:blueviolet; color:white;">&nbsp;&nbsp; Privacy Policy
+        </div>
         <div class="card-body w-full">
           <table>
             <thead>
@@ -47,28 +52,44 @@
             <thead>
               <th>
                 <p class="text-black text-left	">
-■ The Company, with the exception of the cases specified in laws and regulations, will not be provided to third parties without obtaining prior to the consent of the personal information.
+                  ■ The Company, with the exception of the cases specified in laws and regulations, will not be provided
+                  to third parties without obtaining prior to the consent of the personal information.
                   <br />
                   <br />
-■ It maintains the accuracy and up-to-date personal information, safe loss of personal information as well as management, tampering, in order to prevent such leaks, we will achieve the necessary and appropriate information security measures.
+                  ■ It maintains the accuracy and up-to-date personal information, safe loss of personal information as
+                  well as management, tampering, in order to prevent such leaks, we will achieve the necessary and
+                  appropriate information security measures.
                   <br />
                   <br />
-■ Personal information that we acquired, purpose of use shown in when it is acquired or, at the same within the range of reasonable relevance, will be available as long as is necessary on the performance of the business.
-<br />
-In addition, the personal information to interoperate with third parties, or, if you want to entrust the handling of personal information to a third party, provide the proper use of personal information about the joint use of the other party and third party make director for.
+                  ■ Personal information that we acquired, purpose of use shown in when it is acquired or, at the same
+                  within the range of reasonable relevance, will be available as long as is necessary on the performance
+                  of the business.
+                  <br />
+                  In addition, the personal information to interoperate with third parties, or, if you want to entrust
+                  the handling of personal information to a third party, provide the proper use of personal information
+                  about the joint use of the other party and third party make director for.
                   <br />
                   <br />
-■ The Company will comply with laws and other regulations related to personal information protection and proper handling of personal information.
+                  ■ The Company will comply with laws and other regulations related to personal information protection
+                  and proper handling of personal information.
                   <br />
                   <br />
-■ When the Company to acquire the personal information is to endeavor to clarify the purpose of use, by appropriate and fair means, we will obtain personal information.
+                  ■ When the Company to acquire the personal information is to endeavor to clarify the purpose of use,
+                  by appropriate and fair means, we will obtain personal information.
                   <br />
                   <br />
-■ The Company, in person for personal information, it is recognized that has the right to ask for such disclosure, correction, suspension of use and erase, you can set up a personal information consultation, if there these requests, in accordance with laws and regulations promptly we will correspond.                  <br />
+                  ■ The Company, in person for personal information, it is recognized that has the right to ask for such
+                  disclosure, correction, suspension of use and erase, you can set up a personal information
+                  consultation, if there these requests, in accordance with laws and regulations promptly we will
+                  correspond. <br />
                   <br />
-■ The Company will establish a system for implementing the proper handling of personal information to be used on business.                  <br />
+                  ■ The Company will establish a system for implementing the proper handling of personal information to
+                  be used on business. <br />
                   <br />
-■ The Company, in order to run this Privacy Policy, we have established a personal information protection compliance program, which was run by well-known thorough in the company through training and education, by continuous improvement, always best We will maintain the state.                </p>
+                  ■ The Company, in order to run this Privacy Policy, we have established a personal information
+                  protection compliance program, which was run by well-known thorough in the company through training
+                  and education, by continuous improvement, always best We will maintain the state.
+                </p>
               </th>
             </thead>
           </table>
@@ -76,6 +97,6 @@ In addition, the personal information to interoperate with third parties, or, if
         </div>
       </div>
     </div>
-<br />
+    <br />
   </div>
 </template>
