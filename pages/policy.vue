5a7daf3c28821c24c6be370b80c26e9287dc99bc
Quart11 <sitthichot.01sp@gmail.com>
2022-08-04 16:11:35 +0700

    Add policy

---
+++ pages/policy.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+
+    <div class="container mx-auto">
+
+      <div class="border shadow-2xl mockup-window bg-violet-600">
+        <div class="flex justify-center px-4 py-16 text-xl text-black shadow-2xl bg-slate-200">
+          <br />
+
+          <table class="justify-center w-full table-auto ">
+            <thead>
+              <tr>
+                <th class="w-full "></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="content-center text-3xl text-center">Privacy Policy</td>
+              </tr>
+              <br />
+              <tr>
+                <td class="text-xl text-center">Excellence Inc. Ltd. (hereinafter referred to as the Company) will understand that it is the assets personal information is important in advanced information communication society of today, able to handle personal information properly and is recognized as one of our important responsibility , we will declare that strive to protect personal information based on the following policy.</td>
+              </tr>
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+
+  </div>
+  <br />
+
+<!-- Another box -->
+
+
+
+  </div>
+</template>
