5ae6dbf192be22237aaea933ca1a6825d268eccd
Quart11 <sitthichot.01sp@gmail.com>
2022-08-05 11:01:09 +0700

    Add campus recruiting

---
+++ pages/campus.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+
+<!-- First Box -->
+
+    <div class="flex justify-center mx-auto">
+      <div class="justify-center overflow-hidden rounded shadow-2xl bg-slate-50 w-11/12">
+        <div class="card-header text-xl" style="background-color:blueviolet; color:white;">&nbsp;&nbsp; Campus Recruiting
+        </div>
+        <div class="card-body w-full">
+          <table>
+            <thead>
+              <th class="text-black w-80">
+              </th>
+            </thead>
+          </table>
+          <br />
+          <table>
+            <thead>
+              <tr>
+                              <th>
+                <p class="text-black text-left text-2xl">Creation of new business</p>
+              </th>
+              </tr>
+            </thead>
+                          <br />
+
+            <tbody>
+              <tr>
+                <p class="text-black text-left	">
+The current world economy is a difficult situation that can predict.
+Goods which they sell now may also well be that tomorrow will not sell.
+                  <br />
+For this reason, it is keep thinking every day the idea of new business all employees in our company,
+                  <br />
+I believe life work necessary in order to perpetuate the company.
+                  <br />
+                  <br />
+                  <br />
+Everyone not be done, to create their own services, which can provide dreams and excitement to many people.
+                  <br />
+Plan from the discovery of business opportunities, start-up, and go actually profitable.
+One I would like to create a new business model from the idea.
+                  <br />
+We welcome those who are such a enthusiasm and motivation.
+                </p>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+<!-- 2nd Box -->
+
+
+  </div>
+</template>
