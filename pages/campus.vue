d09cbb61cffd66fd7a140b6df3c1cf488bc6b505
Quart11 <sitthichot.01sp@gmail.com>
2022-08-05 13:03:48 +0700

    Edit campus

--- pages/campus.vue
+++ pages/campus.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
 
-<!-- First Box -->
+    <!-- First Box -->
 
     <div class="flex justify-center mx-auto">
-      <div class="justify-center overflow-hidden rounded shadow-2xl bg-slate-50 w-11/12">
-        <div class="card-header text-xl" style="background-color:blueviolet; color:white;">&nbsp;&nbsp; Campus Recruiting
+      <div class="justify-center overflow-hidden rounded shadow-2xl bg-slate-50 w-9/12">
+        <div class="card-header text-xl" style="background-color:blueviolet; color:white;">&nbsp;&nbsp; Campus
+          Recruiting
         </div>
         <div class="card-body w-full">
           <table>
@@ -18,31 +19,32 @@
           <table>
             <thead>
               <tr>
-                              <th>
-                <p class="text-black text-left text-2xl">Creation of new business</p>
-              </th>
+                <th>
+                  <p class="text-black text-left text-2xl">Creation of new business</p>
+                </th>
               </tr>
             </thead>
-                          <br />
+            <br />
 
             <tbody>
               <tr>
                 <p class="text-black text-left	">
-The current world economy is a difficult situation that can predict.
-Goods which they sell now may also well be that tomorrow will not sell.
+                  The current world economy is a difficult situation that can predict.
+                  Goods which they sell now may also well be that tomorrow will not sell.
                   <br />
-For this reason, it is keep thinking every day the idea of new business all employees in our company,
+                  For this reason, it is keep thinking every day the idea of new business all employees in our company,
                   <br />
-I believe life work necessary in order to perpetuate the company.
+                  I believe life work necessary in order to perpetuate the company.
                   <br />
                   <br />
                   <br />
-Everyone not be done, to create their own services, which can provide dreams and excitement to many people.
+                  Everyone not be done, to create their own services, which can provide dreams and excitement to many
+                  people.
                   <br />
-Plan from the discovery of business opportunities, start-up, and go actually profitable.
-One I would like to create a new business model from the idea.
+                  Plan from the discovery of business opportunities, start-up, and go actually profitable.
+                  One I would like to create a new business model from the idea.
                   <br />
-We welcome those who are such a enthusiasm and motivation.
+                  We welcome those who are such a enthusiasm and motivation.
                 </p>
               </tr>
             </tbody>
@@ -50,9 +52,105 @@ We welcome those who are such a enthusiasm and motivation.
         </div>
       </div>
     </div>
+    <br />
 
-<!-- 2nd Box -->
+    <!-- 2nd Box -->
 
+    <div class="container w-9/12 mx-auto">
+
+      <h1 class="text-lg text-center text-white bg-violet-800 ">Company Overview</h1>
+      <div class="relative overflow-x-auto shadow-md ">
+        <table class="w-full text-sm text-left text-white dark:text-gray-400">
+          <thead class="text-xs text-white uppercase dark:text-gray-400">
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="col" class="px-6 py-3 text-white dark:bg-violet-600 auto-cols-auto">
+                Company Name
+              </th>
+              <th scope="col" class="px-6 py-3 text-black bg-blue-300">
+                Excellence Inc. Ltd.
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Location
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                2F Animart-Nishiki 311 Uradeyama-cho KarasumaNishiIru Nishikikogi-Tohri Nakagyo-ku Kyoto-city Kyoto
+                604-8115 Japan
+              </td>
+
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Establishment
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                1989'12 20
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Capital
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                10,000,000
+              </td>
+            </tr>
+
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                President
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Atsunori Tokumasu
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Number of Employees
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                24 (2021'3 data)
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Member Organizations
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Kyoto Venture Business Club
+                <br />
+                Kyoto chamber of commerce Kyoto
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Bank
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Kyoto-Chuo Credit Union
+                <br />
+                Kyoto Bank
+                <br />
+                Kyoto Credit Union
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Business Registration
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                派２６－３００５３２ 一般労働者派遣事業者免許(Japan)
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+
+    </div>
 
   </div>
 </template>
