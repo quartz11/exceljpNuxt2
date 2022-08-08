91a84c0c101c9f1da0804fae8dadceb15d54ad45
Quart11 <sitthichot.01sp@gmail.com>
2022-08-08 16:21:54 +0700

    Add Dispatch

---
+++ pages/B1Dispatch.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+
+    <!-- First Box-->
+
+    <div class="flex justify-center mx-auto">
+      <div class="justify-center overflow-hidden rounded bg-transparent w-11/12">
+        <div class="card-body w-full">
+          <table>
+            <thead>
+              <tr>
+                <th class="text-black w-80 text-left text-2xl">
+                  Dispatch the IT technician
+                </th>
+              </tr>
+            </thead>
+            <br />
+            <tbody>
+              <tr>
+                <td class=" text-left bg-violet-500 text-white text-xl px-6 shadow-2xl">
+                  <br />
+                  To experienced person from rookie, we will deliver superior IT technician.
+                  <br />
+                  (Kyoto, Osaka, Shiga, Tokyo and overseas)
+                  <br />
+                  <br />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <!-- 2nd Box -->
+
+    <div class="flex justify-center mx-auto">
+      <div class="justify-center overflow-hidden rounded bg-transparent w-11/12">
+        <div class="card-body w-full">
+          <table>
+            <thead>
+              <tr>
+                <th>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class=" text-left bg-blue-400 text-black text-xl px-6 h-10">
+                  From rookie to experienced person
+                </td>
+              </tr>
+              <tr>
+                <td class="text-left bg-slate-50 text-black text-lg px-6">
+                  <br />
+                  Technique level and results years, budget, period and number of persons, age, etc.,
+                  <br />
+                  It is possible that I am allowed to support us to flexible on request.
+                  <br />
+                  Technical field and language also does not matter.
+                  <br />
+                  Please do not hesitate to contact us.
+                  <br />
+                  <br />
+                </td>
+              </tr>
+              <tr>
+                <td class=" text-left bg-blue-400 text-black text-xl px-6 h-10">
+                  Employee training: Now what is asked “human force”
+                </td>
+              </tr>
+              <tr>
+                <td class="text-left bg-slate-50 text-black text-lg px-6">
+                  <br />
+                  The training of the “human force”, we have especially emphasized.
+                  <br />
+                  That it is a cooperative do our thanks to the other party, that it is a sincere, doing a neat
+                  information transmission.
+                  <br />
+                  Pull out the power of the team to the maximum, in order to lead to a successful project,
+                  <br />
+                  We are focused on human force training.
+                  <br />
+                  <br />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+<br />
+<!-- Business Footer -->
+<div class="flex justify-center mx-auto w-1/3">
+<table>
+  <thead>
+    <tr>
+      <th class="text-black text-center text-2xl">
+Introduction
+      </th>
+    </tr>
+  </thead>
+  <br />
+  <tbody class="text-center">
+    <tr>
+      <td>
+            <nuxt-link to="/B1Dispatch"><p class="text-black text-lg">Dispatch the IT technician</p></nuxt-link>
+      </td>
+    </tr>
+        <tr>
+      <td>
+            <nuxt-link to="/"><p class="text-black text-lg">Development Package</p></nuxt-link>
+      </td>
+    </tr>
+            <tr>
+      <td>
+            <nuxt-link to="/"><p class="text-black text-lg">System Outsourcing</p></nuxt-link>
+      </td>
+    </tr>
+            <tr>
+      <td>
+            <nuxt-link to="/"><p class="text-black text-lg">Offshore Development</p></nuxt-link>
+      </td>
+    </tr>
+            <tr>
+      <td>
+            <nuxt-link to="/"><p class="text-black text-lg">Asia foray consultants</p></nuxt-link>
+      </td>
+    </tr>
+            <tr>
+      <td>
+            <nuxt-link to="/"><p class="text-black text-lg">Translation(Japanese/English/Chinese/Thai)</p></nuxt-link>
+      </td>
+    </tr>
+            <tr>
+      <td>
+            <nuxt-link to="/"><p class="text-black text-lg">Siam Order Service(Import agency)</p></nuxt-link>
+      </td>
+    </tr>
+            <tr>
+      <td>
+            <nuxt-link to="/"><p class="text-black text-lg">Other Development Results</p></nuxt-link>
+      </td>
+    </tr>
+  </tbody>
+</table>
+</div>
+
+  </div>
+</template>
