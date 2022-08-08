9290305bc78ea912942b6266895589c69e95f462
Rinku Inukowa <inukowarinku@gmail.com>
2022-08-08 17:08:58 +0700

    translate index

--- assets/lang/en.js
+++ assets/lang/en.js
@@ -1,24 +1,52 @@
-export default{
-    welcome: 'Welcome',
-    navbar: {
-        home: {
-            home: 'Home',
-            news: 'News',
-        },
-        overview: {
-            overview: 'Overview',
-            company:'Company Overview',
-            history:'Our History',
-            philosophy:'Our Philosophy',
-            greeting:'Greeting',
-            employment:'Employment',
-        },
-        business: {
-            business: 'Our Business',
-            product:'Our Product',            
-        },
-        joinus:'Join us', 
-        contact:'Contact us', 
-        
+export default {
+  welcome: "Welcome",
+  navbar: {
+    home: {
+      home: "Home",
+      news: "News",
     },
-}
+    overview: {
+      overview: "Overview",
+      company: "Company Overview",
+      history: "Our History",
+      philosophy: "Our Philosophy",
+      greeting: "Greeting",
+      employment: "Employment",
+    },
+    business: {
+      business: "Our Business",
+      product: "Our Product",
+    },
+    joinus: "Join us",
+    contact: "Contact us",
+  },
+  index: {
+    pa_1: `"The pursuit of happiness discretion both sides of all employees. 
+        To provide the best of ICT services to customers, 
+        It will contribute to the realization of a livable peace society."`,
+    pa_2: `We will continue to all employees growth beneath the the
+        management philosophy. And we have stacked the day-to-day study in order to continue to be a company
+        that will contribute to the world.
+        Also,beneath the idea of“Making the first person before you make things.”we have the human resources
+        development as one of the important themes.
+        Learn the technology, to learn philosophy.Then growing employees voluntarily each person was, and will
+        continue to further growth by learn sometimes gathered.
+        Its employees and by mutual cooperation, the force will be the companies able to contribute to the world
+        become a greater force.`,
+    card_head: `Partners Recruiting.`,
+    card_title: `We are looking for a partner.`,
+    card_text: `The Company is looking for a partner who can both perform software
+    development.
+    Interested in our business areas, if there is a possibility you'll find cooperation, thank you for your
+    consideration.
+    We are, for the sake of technology provided by customers satisfaction is, without having to stick to our
+    own,
+    widely flexibility in seeking business partners, and we believe Let's build a relationship of
+    collaboration.
+
+    And entered into a partnership for the purpose acquisition and the business strengthening of business
+    opportunities
+    both to expand business opportunities by sharing technology and know-how of each other, why not continue
+    to grow together.`
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
 
-    <div>
+    <!-- <div>
       <h2>
         TEST LANGUAGE Welcome - {{ $t('welcome') }}
         <br />
         TEST LANGUAGE Home - {{ $t('navbar.home.home') }}
       </h2>
-    </div>
+    </div> -->
 
     <section class="py-5">
       <div class="container mx-auto">
@@ -17,10 +17,9 @@
             <!-- CUSTOM BLOCKQUOTE -->
             <blockquote class="p-5 bg-white rounded shadow-2xl blockquote blockquote-custom">
               <div class="shadow-2xl blockquote-custom-icon bg-info"></div>
-              <p class="mt-2 mb-0 text-black font-italic">"The pursuit of happiness discretion both sides of all
-                employees.
-                To provide the best of ICT services to customers,
-                It will contribute to the realization of a livable peace society."</p>
+              <p class="mt-2 mb-0 text-black font-italic content-center">
+                {{ $t('index.pa_1') }}
+              </p>
 
             </blockquote>
             <!-- END -->
@@ -39,15 +38,8 @@
             <!-- CUSTOM BLOCKQUOTE -->
             <blockquote class="p-5 bg-white rounded shadow-2xl blockquote blockquote-custom">
               <div class="shadow-2xl blockquote-custom-icon bg-info"></div>
-              <p class="mt-2 mb-0 text-black font-italic">We will continue to all employees growth beneath the the
-                management philosophy. And we have stacked the day-to-day study in order to continue to be a company
-                that will contribute to the world.
-                Also,beneath the idea of“Making the first person before you make things.”we have the human resources
-                development as one of the important themes.
-                Learn the technology, to learn philosophy.Then growing employees voluntarily each person was, and will
-                continue to further growth by learn sometimes gathered.
-                Its employees and by mutual cooperation, the force will be the companies able to contribute to the world
-                become a greater force.
+              <p class="mt-2 mb-0 text-black font-italic">
+                {{ $t('index.pa_2') }}
               </p>
 
             </blockquote>
@@ -63,53 +55,43 @@
       <div class="mx-auto col-lg-6">
 
         <div class="w-full overflow-hidden rounded shadow-2xl bg-slate-50">
-          <div class="card-header" style="background-color:blueviolet; color:white;">&nbsp;&nbsp; Partners Recruiting
+          <div class="card-header" style="background-color:blueviolet; color:white;">
+            &emsp; {{ $t('index.card_head') }}
           </div>
           <div class="card-body">
-            <h5 class="text-black card-title">We are looking for a partner.</h5>
-            <p class="text-black card-text">The Company is looking for a partner who can both perform software
-              development.
-              Interested in our business areas, if there is a possibility you'll find cooperation, thank you for your
-              consideration.
-              We are, for the sake of technology provided by customers satisfaction is, without having to stick to our
-              own,
-              widely flexibility in seeking business partners, and we believe Let's build a relationship of
-              collaboration.
-
-              And entered into a partnership for the purpose acquisition and the business strengthening of business
-              opportunities
-              both to expand business opportunities by sharing technology and know-how of each other, why not continue
-              to grow together.</p>
+            <h5 class="text-black card-title">{{ $t('index.card_title') }}</h5>
+            <p class="text-black card-text">
+              {{ $t('index.card_text') }}
+            </p>
           </div>
         </div>
 
       </div>
       <br />
     </div>
-<br />
+    <br />
 
 
-<div class="flex justify-center">
+    <div class="flex justify-center">
+      <table>
+        <thead>
+          <tr>
+            <th class="w-1/3"></th>
+            <th class="w-1/3">
+              <nuxt-link to="/register">
+                <button
+                  class="w-full	 content-center px-4 py-2 font-bold text-white bg-blue-500 border border-blue-700 rounded hover:bg-blue-700 mx-2">
+                  Inquiry form
+                </button>
+              </nuxt-link>
+            </th>
+            <th class="w-1/3"></th>
+          </tr>
+        </thead>
+      </table>
+    </div>
 
-  <table>
-<thead>
-<tr>
-  <th class="w-1/3"></th>
-  <th class="w-1/3">
-        <nuxt-link to="/register">
-        <button
-          class="w-full	 content-center px-4 py-2 font-bold text-white bg-blue-500 border border-blue-700 rounded hover:bg-blue-700 mx-2">
-          Inquiry form
-        </button>
-      </nuxt-link>
-  </th>
-  <th class="w-1/3"></th>
-</tr>
-</thead>
-</table>
-</div>
-
-<br />
+    <br />
 
   </div>
 </template>
