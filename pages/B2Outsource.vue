90608fc103f29ee3e1d683e81a9f21d169247c35
Quart11 <sitthichot.01sp@gmail.com>
2022-08-08 16:51:34 +0700

    Add outsource

---
+++ pages/B2Outsource.vue
@@ -0,0 +1,186 @@
+<template>
+  <div>
+
+    <!-- First Box-->
+
+    <div class="flex justify-center mx-auto">
+      <div class="justify-center overflow-hidden rounded bg-transparent w-11/12">
+        <div class="card-body w-full">
+          <table>
+            <thead>
+              <tr>
+                <th class="text-black w-80 text-left text-2xl">
+                  System Outsourcing
+                </th>
+              </tr>
+              <tr>
+                <div class="flex justify-center mx-auto">
+                  <img src="http://1excellence.com/official/img/business/given.jpg" width="300" height="300" />
+                </div>
+              </tr>
+            </thead>
+            <br />
+            <tbody>
+              <tr>
+                <td class=" text-left bg-violet-500 text-white text-xl px-6 shadow-2xl">
+                  <br />
+                  Overseas & Japan staff of collaboration by offshore development.
+                  <br />
+                  Low price × security of quality realization is possible.
+                  <br />
+                  <br />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <!-- 2nd Box -->
+
+    <div class="flex justify-center mx-auto">
+      <div class="justify-center overflow-hidden rounded bg-transparent w-11/12">
+        <div class="card-body w-full">
+          <table>
+            <thead>
+              <tr>
+                <th>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class=" text-left bg-blue-400 text-black text-xl px-6 h-10">
+                  Development Results of Excellence
+                </td>
+              </tr>
+              <tr>
+                <td class="text-left bg-slate-50 text-black text-lg px-6">
+                  <br />
+                  Bonded warehouse system and large-scale automated warehouse system, there is a track record of working
+                  on such as hotel reservation system
+                  <nuxt-link to="/product"><a class="text-blue-600">（Click here for details）</a></nuxt-link>.
+                  <br />
+                  <br />
+                </td>
+              </tr>
+              <tr>
+                <td class=" text-left bg-blue-400 text-black text-xl px-6 h-10">
+                  In all fields by experts, thorough and PDCA
+                </td>
+              </tr>
+              <tr>
+                <td class="text-left bg-slate-50 text-black text-lg px-6">
+                  <br />
+                  A variety of industries, terminal, the engineer who engaged in the development of the language has
+                  been gathered.
+                  <br />
+                  We want to meet the many needs even one person.
+                  <br />
+                  Our company is in this thought, we accept your order regardless of the field.
+                  <br />
+                  We are pursuing the absolute quality by experts in each field.
+                  <br />
+                  <br />
+                </td>
+              </tr>
+              <tr>
+                <td class=" text-left bg-blue-400 text-black text-xl px-6 h-10">
+                  The secret of the low price?
+                </td>
+              </tr>
+              <tr>
+                <td class="text-left bg-slate-50 text-black text-lg px-6">
+                  <br />
+                  Overseas resides staff we have also deployed.
+                  <br />
+                  <nuxt-link to="/offshoe"><a class="text-blue-600 underline">Offshore Development</a></nuxt-link> With
+                  the introduction, depending on the development scale and budget of the customer's wish, it is possible
+                  to respond flexibly.
+                  <br />
+                  The joint development with Japan of staff are based, we will guarantee quality standards maintained.
+                  <br />
+                  <br />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <br />
+    <!-- Business Footer -->
+    <div class="flex justify-center mx-auto w-1/3">
+      <table>
+        <thead>
+          <tr>
+            <th class="text-black text-center text-2xl">
+              Introduction
+            </th>
+          </tr>
+        </thead>
+        <br />
+        <tbody class="text-center">
+          <tr>
+            <td>
+              <nuxt-link to="/B1Dispatch">
+                <p class="text-black text-lg">Dispatch the IT technician</p>
+              </nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <nuxt-link to="/">
+                <p class="text-black text-lg">Development Package</p>
+              </nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <nuxt-link to="/">
+                <p class="text-black text-lg">System Outsourcing</p>
+              </nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <nuxt-link to="/">
+                <p class="text-black text-lg">Offshore Development</p>
+              </nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <nuxt-link to="/">
+                <p class="text-black text-lg">Asia foray consultants</p>
+              </nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <nuxt-link to="/">
+                <p class="text-black text-lg">Translation(Japanese/English/Chinese/Thai)</p>
+              </nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <nuxt-link to="/">
+                <p class="text-black text-lg">Siam Order Service(Import agency)</p>
+              </nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <nuxt-link to="/">
+                <p class="text-black text-lg">Other Development Results</p>
+              </nuxt-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+  </div>
+</template>
