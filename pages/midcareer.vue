3b9dc4af59fd6f4a368a1b53471e9a22d881eb78
Quart11 <sitthichot.01sp@gmail.com>
2022-08-08 10:57:56 +0700

    Add mid career

--- pages/midcareer.vue
+++ pages/midcareer.vue
@@ -1,7 +1,409 @@
 <template>
   <div>
 
+    <!-- First Box -->
 
+    <div class="flex justify-center mx-auto">
+      <div class="justify-center overflow-hidden rounded shadow-2xl bg-slate-50 w-9/12">
+        <div class="card-header text-xl" style="background-color:blueviolet; color:white;">&nbsp;&nbsp; Mid-career
+          recruitment
+        </div>
+        <div class="card-body w-full">
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  <p class="text-black text-left text-2xl">Creation of new business</p>
+                </th>
+              </tr>
+            </thead>
+            <br />
+
+            <tbody>
+              <tr>
+                <p class="text-black text-left	">
+                  The current world economy is a difficult situation that can predict.
+                  Goods which they sell now may also well be that tomorrow will not sell.
+                  <br />
+                  For this reason, it is keep thinking every day the idea of new business all employees in our company,
+                  <br />
+                  I believe life work necessary in order to perpetuate the company.
+                  <br />
+                  <br />
+                  <br />
+                  Everyone not be done, to create their own services, which can provide dreams and excitement to many
+                  people.
+                  <br />
+                  Plan from the discovery of business opportunities, start-up, and go actually profitable.
+                  One I would like to create a new business model from the idea.
+                  <br />
+                  We welcome those who are such a enthusiasm and motivation.
+                </p>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <br />
+
+    <!-- 2nd Box -->
+
+    <div class="container w-9/12 mx-auto">
+
+      <h1 class="text-lg text-left text-white bg-violet-800 ">&nbsp;&nbsp; Adoption condition</h1>
+      <div class="relative overflow-x-auto shadow-md ">
+        <table class="w-full text-sm text-left text-white dark:text-gray-400">
+          <thead class="text-xs text-white dark:text-gray-400">
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="col" class="px-6 py-3 text-sm	 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Job category
+              </th>
+              <th scope="col" class="px-6 py-3 text-black text-sm	 font-medium bg-blue-300">
+                Programmers, system engineers, system sales
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Employment status
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Full-time, and contract employees by hope
+
+              </td>
+
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Job description
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                ◉ Setting, development, maintenance, operation and management of computer systems.
+                <br />
+                ◉ Design and development of software.
+                <br />
+                ◉ Design and development of network.
+                <br />
+                ◉ Building a Web site, production.
+              </td>
+
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Human resources to find
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                ◉ Mind and body hale and be an honest, there is some physical strength.
+                <br />
+                ◉ I like to read the newspapers and history books, etc., and hear properly Some people talk.
+                <br />
+                ◉ Value a originality and thinking in my head. (it is not blindly rely on Google)
+                <br />
+                ◉ There is some strong ambition to pressure.
+                <br />
+                ◉ Coordination ability high, people with high contribution willingness to society.
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Skills conditions
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                ◉ Especially people who want to do face recognition technology development in crime prevention for the
+                camera, and the like.
+                <br />
+                ◉ Security systems product development seekers in RFID + GPS and the like.
+                <br />
+                ◉ Sales experience in the software industry.
+                <br />
+                ◉ Infrastructure engineer (Unix Linux various WindowsServer)
+                <br />
+                ◉ Network engineer
+                <br />
+                ◉ Expert of RDB (Oracle, SQLServer, DB2, etc.)
+                <br />
+                ◉ C #, C ++, VB.NET, development experience in EJB, etc.
+                <br />
+                ◉ JSP & Servlet, Web-based development experience, such as ASP
+                <br />
+                ◉ Development experience of embedded system and the control system
+                <br />
+                ◉ Basic information processing engineer of the Ministry of Economy, Trade and Industry, software development engineer of qualified personnel is preferred.
+              </td>
+            </tr>
+
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Wanted persons
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Some persons
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Target
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Graduate school, university, computer technical colleges already graduated
+                <br />
+                Work experience about three years or more, or people having the same productivity.
+                <br />
+                Experienced even those of foreign nationality OK (1 grade or more Japanese)
+                <br />
+                Work experience about 5 years or more, or people having the same productivity. (contract employees)
+                <br />
+                Human experience as sales of the proposed sales and technician dispatch of system development projects there are more than 3 years.
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+
+    </div>
+
+    <!-- 3rd Box-->
+
+    <div class="container w-9/12 mx-auto">
+
+      <h1 class="text-lg text-left text-white bg-violet-800 ">&nbsp;&nbsp; Treatment</h1>
+      <div class="relative overflow-x-auto shadow-md ">
+        <table class="w-full text-sm text-left text-white dark:text-gray-400">
+          <thead class="text-xs text-white dark:text-gray-400">
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="col" class="px-6 py-3 text-sm text-white dark:bg-violet-600 auto-cols-auto">
+                Social insurance
+              </th>
+              <th scope="col" class="px-6 py-3 text-black text-sm	 font-normal	 bg-blue-300">
+                Conditioning (employment insurance, workers' compensation insurance, health insurance, welfare
+                pensioninsurance) </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Wage pattern
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Monthly salary system (end of the month deadline, 15th of the following month payment)
+              </td>
+
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Base salary </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                ※Determine individually depending on the experience and track record and ability. (Negotiable)
+
+                <br />
+                200,000 yen to 400,000 yen monthly salary
+
+                <br />
+(Reference monthly remuneration of contract employees: 300,000 yen to 600,000 yen)
+                <br />
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Raise and bonus
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Raise: once a year (in April), bonus: twice a year (July / December) according to the previous year.
+                <br />
+                (Contract employees: salary increase, no bonus, and included in the payroll)
+                <br />
+              </td>
+            </tr>
+
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Various allowances
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Commuting allowance.
+                <br />
+                Duty allowance.
+                <br />
+                Overtime pay.
+                <br />
+                Qualification celebration allowance.
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Holiday-Vacation
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                ◉ Full five-day week. (Saturday, Sunday and public holidays)
+                <br />
+                ◉ Annual vacation days about 120 days.
+                <br />
+                ※Other: company designated date, New Year · GW · summer vacation, annual paid leave, etc.
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Working hours
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                9:00 to 18:00 (break 1 hour)
+                <br />
+                ※There vary slightly depending resident destination.
+
+              </td>
+
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Work location
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Headquarters (after training ended Kyoto headquarters, Kyoto, Osaka and Shiga customer destination)
+              </td>
+
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                System
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                Qualification incentive system.
+                <br />
+                congratulatory or condolence payments.
+              </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-white dark:bg-violet-600 auto-cols-auto">
+                Remarks
+              </th>
+              <td class="px-6 py-4 text-black bg-blue-300">
+                ◉ On-site work in the resident destination becomes the main.
+                <br />
+                ◉ Resident destination is within the range from home within one way about an hour and a half.
+                <br />
+                ◉ After joining is done the new employee training in Kyoto headquarters.
+                <br />
+                ◉ Tokyo, China, Southeast Asia, will unlikely be a business trip to the United States.(short-term - long
+                term)
+                <br />
+                ◉ There trial period. (3 months to 6 months depending on the situation)
+                <br />
+                ◉ English a plus those who speak.
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+    </div>
+
+    <!-- 4th Box -->
+
+    <div class="flex justify-center mx-auto">
+      <div class="justify-center overflow-hidden rounded shadow-2xl bg-slate-50 w-9/12">
+        <div class="card-header text-xl" style="background-color:blueviolet; color:white;">&nbsp;&nbsp; Documents to be
+          submitted</div>
+        <div class="card-body w-full">
+          <table>
+            <thead>
+              <tr>
+                <th>
+                </th>
+              </tr>
+            </thead>
+            <br />
+
+            <tbody>
+              <tr>
+                <p class="text-black text-left	">
+                  Please mail the following documents to us.
+                </p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  ※It should be noted that, at the time of application will be made to read our “Privacy Policy”, please
+                  your application on top of the agreement. </p>
+              </tr>
+              <br />
+              <tr>
+                <p class="text-black text-left	">
+                  1.With handwriting photo resume (A4, described the best match) ※ word processor writing invalid</p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  2.Expected graduation certificate (graduation certificate in the case of already graduated) </p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  3.Transcript </p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  4.Within three months of health certificate (copy available) </p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  5.Motivation letter (about 400 words) </p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  6.The contents of the graduation study (400 words max) </p>
+              </tr>
+              <br />
+              <tr>
+                <p class="text-black text-left	">
+                  Destination </p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  ◉ 〒604-8155
+                  <br />
+                  &nbsp;&nbsp;&nbsp;&nbsp; Kyoto Nakagyo-ku Uradeyama cho address 311 Animato Nishiki 2F
+                  <br />
+                  &nbsp;&nbsp;&nbsp;&nbsp; Recruitment Officer: Tokumasu Atsunori
+                </p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  ◉ TEL:075-256-7872
+                </p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  ◉ FAX:075-256-9052
+                </p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  ◉ mail: recruit@1excellence.com
+                </p>
+              </tr>
+              <br />
+              <tr>
+                <p class="text-black text-left	">
+                  Reception time </p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  ◉ 9:00-18:00Excluding weekends and holidays</p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  Remarks </p>
+              </tr>
+              <tr>
+                <p class="text-black text-left	">
+                  ◉ Please contact feel free to recruiters If you have any unknown point or questions.</p>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <br />
 
   </div>
 </template>
