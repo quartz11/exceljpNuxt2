fcc2542d26a0d25581504b946f2cfffb72170efa
Quart11 <sitthichot.01sp@gmail.com>
2022-08-08 14:10:06 +0700

    Add employee interview 1

--- pages/empinter1.vue
+++ pages/empinter1.vue
@@ -1,7 +1,169 @@
 <template>
   <div>
 
+    <!-- First Box -->
+
+    <div class="flex justify-center mx-auto">
+      <div class="justify-center overflow-hidden rounded shadow-2xl bg-slate-50 w-11/12">
+        <div class="card-body w-full">
+          <table>
+            <thead>
+              <th class="text-black w-80">
+                <div class="flex justify-center">
+                  <img width="150" height="150" src="http://1excellence.com/official/img/recruit/shimizu.gif" />
+                </div>
+                <br />
+                <p>System Development Department</p>
+                <br />
+                Shimizu Yasuyuki
+              </th>
+              <th class="w-screen text-black">
+                <p class="text-2xl">
+                  Mind (thinking) is important, as long as what the firm to be called should there as a person, you will
+                  be able to grow reliably.
+                </p>
+              </th>
+            </thead>
+          </table>
+        </div>
+      </div>
+    </div>
+    <br />
+    <!-- Table -->
 
 
+    <div class="flex justify-center mx-auto">
+      <div class="justify-center overflow-hidden rounded bg-transparent w-11/12">
+        <div class="card-body w-full">
+          <table>
+            <thead>
+              <th class="text-black w-80">
+              </th>
+            </thead>
+
+            <tbody>
+              <tr>
+                <th class="bg-blue-600 text-white text-left h-8	">
+                  &nbsp; What is the motivation that was joined to this company?
+                </th>
+              </tr>
+              <tr>
+                <th class="bg-slate-50 text-black text-left">
+                  <br />
+                  &nbsp; Is it personality of the The company president.I think that it is felt if the conversation
+                  toward the surface.In addition, the playing field even things and new employees that are focused on
+                  education that is given now to the direct motivation.
+                  <br />
+                  <br />
+                </th>
+              </tr>
+              <tr>
+                <th class="bg-blue-600 text-white text-left h-8	">
+                  &nbsp; What do you have to work now?
+
+                </th>
+
+              </tr>
+              <tr>
+                <th class="bg-slate-50 text-black text-left">
+                  <br />
+                  &nbsp; Was seconded to the customer destination, you have a Windows application developed in C #
+                  language.We are developing a map-related applications.
+                  <br />
+                  <br />
+                </th>
+              </tr>
+              <tr>
+                <th class="bg-blue-600 text-white text-left h-8	">
+                  &nbsp; What did you learn after joining?
+                </th>
+
+              </tr>
+              <tr>
+                <th class="bg-slate-50 text-black text-left">
+                  <br />
+                  &nbsp; It is a philosophy (management philosophy).We hold regular study sessions as employee
+                  education, but not things like increase the technical capabilities, study of philosophy has become the
+                  center.Technical strength also is important in terms of going to work, but can be worn in the
+                  work.First heart (idea) is important, if what the firm to be called should there as a person, we
+                  believe that it can be grown reliably.
+                  <br />
+                  <br />
+                </th>
+              </tr>
+              <tr>
+                <th class="bg-blue-600 text-white text-left h-8	">
+                  &nbsp; What is that you are left in the most impressive in the past?
+                </th>
+
+              </tr>
+              <tr>
+                <th class="bg-slate-50 text-black text-left">
+                  <br />
+                  &nbsp; Hundreds of thousands it is also that it was training I am allowed to participate in, such
+                  as.In our company, we are focused on the idea in education and that individual is directly linked to
+                  that company to grow if growth.Content is also often made for themselves, it will be able to
+                  participate in various training if they wish.
+                  <br />
+                  <br />
+                </th>
+              </tr>
+              <tr>
+                <th class="bg-blue-600 text-white text-left h-8	">
+                  &nbsp; Message to students in job hunting
+                </th>
+
+              </tr>
+              <tr>
+                <th class="bg-slate-50 text-black text-left">
+                  <br />
+                  &nbsp; In our company, it can cause the proposals and act in his power even new employees.If you are
+                  motivated, let's do our best come together.
+                  <br />
+                  <br />
+                </th>
+              </tr>
+              <tr>
+                <th class="bg-blue-600 text-white text-left h-8	">
+                  &nbsp; Finally, anything you want it and you want that oysters say.
+                </th>
+
+              </tr>
+              <tr>
+                <th class="bg-slate-50 text-black text-left">
+                  <br />
+                  &nbsp; I think there are many things to be worried about, such as business content and work location,
+                  but I think also people working together is not the most important.I think that I want you to cherish
+                  the company select the impression you feel Try actually met.
+                  <br />
+                  <br />
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <br />
+
+    <!-- Other interview -->
+    <div class="flex justify-center mx-auto">
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <nuxt-link to="/">
+                <p class="text-xl text-blue-600 underline">See an interview with other employees</p>
+              </nuxt-link>
+            </th>
+          </tr>
+          <tr>
+            <th>
+              <p class="text-black">(Display every time you click will change)</p>
+            </th>
+          </tr>
+        </thead>
+      </table>
+    </div>
   </div>
 </template>
