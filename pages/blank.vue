9c09427d81811d6435ebf7b0f2f7ddc8629666e7
Quart11 <sitthichot.01sp@gmail.com>
2022-08-09 17:03:22 +0700

    Add blank page

---
+++ pages/blank.vue
@@ -0,0 +1,205 @@
+<template>
+  <div>
+
+    <!-- First Box -->
+    <div class="flex mx-auto w-3/4 shadow-2xl">
+      <div class="card w-full">
+        <div class="card-body bg-slate-50">
+          <h1 class="px-4 text-2xl text-black text-left font-bold">特定商取引法に基づく表記</h1>
+          <br />
+          <p class="px-4 text-sm text-black text-left">「特定商取引に関する法律」（旧 通信販売法）・通信販売（通信販売についての広告）第十一条に基づき以下に明示致します。</p>
+          <br />
+          <br />
+          <table>
+            <thead>
+              <tr>
+
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="w-32">
+                  <p class="text-black px-4">販売事業者 </p>
+                </td>
+                <td>
+                  <p class="text-black px-4">エクセレンス株式会社</p>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-32">
+                  <p class="text-black px-4">運営統括責任者 </p>
+                </td>
+                <td>
+                  <p class="text-black px-4">徳増 篤律</p>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-32">
+                  <p class="text-black px-4">所在地</p>
+                </td>
+                <td>
+                  <p class="text-black px-4">〒604-8155</p>
+                  <p class="text-black px-4">京都府京都市中京区錦小路通烏丸西入占出山町311 アニマート錦 2F</p>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-32">
+                  <p class="text-black px-4">電話番号 </p>
+                </td>
+                <td>
+                  <p class="text-black px-4">075-256-7872</p>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-32">
+                  <p class="text-black px-4">メールアドレス </p>
+                </td>
+                <td>
+                  <p class="text-black px-4">excel@mbox.kyoto-inet.or.jp</p>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-32">
+                  <p class="text-black px-4">サービス内容 </p>
+                </td>
+                <td>
+                  <p class="text-black px-4">個人輸入代行サービス</p>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-32">
+                  <p class="text-black px-4">販売価格 </p>
+                </td>
+                <td>
+                  <p class="text-black px-4">商品毎に表示</p>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-32">
+                  <p class="text-black px-4">商品代金以外の必要金額 </p>
+                </td>
+                <td>
+                  <p class="text-black px-4">送料・決済手数料・税金等</p>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-32">
+                  <p class="text-black px-4">商品引渡し方法 </p>
+                </td>
+                <td>
+                  <p class="text-black px-4">ご入金後、商品入荷次第、タイ事務所にてタイ国より発送いたします。発送から約3～7日程度で到着いたします。</p>
+                  <p class="text-black px-4">※商品の流通事情により発送が遅れる場合があります。</p>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-32">
+                  <p class="text-black px-4">お支払い方法 </p>
+                </td>
+                <td>
+                  <p class="text-black px-4">銀行振込・PayPal</p>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-32">
+                  <p class="text-black px-4">返品について </p>
+                </td>
+                <td>
+                  <p class="text-black px-4">お客様都合による返品は一切お受けできません。</p>
+                  <p class="text-black px-4">※納品物に不備（発送間違い、郵送時に被ったとは考えられない汚損や破損）があった場合にはお問い合わせフォームよりご連絡ください。</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <br />
+    <br />
+    <!-- Recommended Box -->
+    <div class="flex justify-center mx-auto">
+      <h1 class="text-black font-bold text-xl text-center">Recommended</h1>
+      <br />
+      <br />
+    </div>
+
+    <div class="flex justify-center mx-auto w-2/5	">
+
+      <div class="hero h-48 bg-slate-50 shadow-2xl">
+        <div class="hero-content flex-col lg:flex-row">
+          <nuxt-link to="/">
+            <img src="http://1excellence.com/official/img/store/gac_sub.png" width="150" height="250"
+              class="max-w-sm rounded-lg shadow-2xl" />
+          </nuxt-link>
+          <div>
+            <h1 class="text-2xl font-bold text-blue-600">GAC juice fruit mix</h1>
+            <br />
+            <p class="text-black">GAC fruit is a “gift from heaven” is now juice.</p>
+            <p class="text-black">It is a mix juice and passion fruit.</p>
+            <p class="text-black">For more information click on the image!</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <br />
+
+    <div class="flex justify-center mx-auto w-2/5	">
+
+      <div class="hero h-56 bg-slate-50 shadow-2xl">
+        <div class="hero-content flex-col lg:flex-row">
+          <nuxt-link to="/">
+            <img src="http://1excellence.com/official/img/store/krachaidum.jpg" width="150" height="250"
+              class="max-w-sm rounded-lg shadow-2xl" />
+          </nuxt-link>
+          <div>
+            <h1 class="text-2xl font-bold text-blue-600">クラチャイダム天然カプセル</h1>
+            <br />
+            <p class="text-black">精力増進や滋養強壮の効果で知られるクラチャイダム。</p>
+            <p class="text-black">クラチャイダムの粉末をカプセルに凝縮しました。</p>
+            <p class="text-black">詳しくは画像をクリック！</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <br />
+    <div class="flex justify-center mx-auto w-2/5	">
+
+      <div class="hero h-72 bg-slate-50 shadow-2xl">
+        <div class="hero-content flex-col lg:flex-row">
+          <nuxt-link to="/">
+            <img src="http://1excellence.com/official/img/store/sopfront.jpg" width="150" height="250"
+              class="max-w-sm rounded-lg shadow-2xl" />
+          </nuxt-link>
+          <div>
+            <h1 class="text-2xl font-bold text-blue-600">サワーソップ ティー</h1>
+            <br />
+            <p class="text-black">がんを抑制すると言われているサワーソップ。</p>
+            <p class="text-black">サワーソップの葉を加工して作られたティーバッグです。</p>
+            <p class="text-black">詳しくは画像をクリック！</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <br />
+    <br />
+    <div class="flex justify-center mx-auto">
+      <nuxt-link to="/blank">
+        <a class="text-blue-600 underline text-xl">特定商取引法に基づく表記</a>
+      </nuxt-link>
+    </div>
+
+
+  </div>
+</template>
+
+<style scoped>
+table,
+th,
+td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+</style>
