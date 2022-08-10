866ff2c42f8edd2e3aacac9b99ffa6d00deb1441
Quart11 <sitthichot.01sp@gmail.com>
2022-08-10 16:45:49 +0700

    Edit business sub pages

--- pages/business.vue
+++ pages/business.vue
@@ -14,7 +14,7 @@
             we are aligned to the skills experienced technicians from the newcomer,Regardless of the field.
             Please contact us.</p>
           <div class="card-actions">
-            <nuxt-link to="/B1Dispatch">
+            <nuxt-link to="/business1/B1Dispatch">
               <button class="btn btn-primary">More info</button>
             </nuxt-link>
           </div>
@@ -31,18 +31,18 @@
             <nuxt-link to="/product"><p class="text-black">Development Package</p></nuxt-link>
           </h2>
           <p class="text-black">In our company we have been developing products for various business.</p>
-          <a href="http://1excellence.com/#Treatment%20support%20application">
+          <nuxt-link to="/products/p1treatment">
             <p class="text-sky-600">Treatment support application</p>
-          </a>
-          <a href="http://1excellence.com/#Bonded%20Warehouse%20System">
+          </nuxt-link>
+          <nuxt-link to="/products/p2warehouse">
             <p class="text-sky-600">Bonded Warehouse System</p>
-          </a>
-          <a href="http://1excellence.com/#Groupware%20System">
-            <p class="text-sky-600">Groupware System</p>
-          </a>
-          <a href="http://1excellence.com/#Hotel%20Reservation%20System">
+          </nuxt-link>
+          <nuxt-link to="/products/p3ebase">
+           <p class="text-sky-600">Groupware System</p>
+          </nuxt-link>
+          <nuxt-link to="/products/p4reservation">
             <p class="text-sky-600">Hotel Reservation System</p>
-          </a>
+          </nuxt-link>
         </div>
       </div>
       <br />
@@ -59,7 +59,7 @@
             In addition, as long as it is a project that is similar to the development track record, it is possible more
             rapid response.</p>
           <div class="card-actions">
-            <nuxt-link to="/B2Outsource">
+            <nuxt-link to="/business1/B2Outsource">
               <button class="btn btn-primary">More info</button>
             </nuxt-link>
           </div>
@@ -77,7 +77,7 @@
             development approach in Thailand　subsidiary.
             The management format by Japanese staff are based, it will simultaneously achieve high quality.</p>
           <div class="card-actions">
-            <nuxt-link to="/B3Offshoe">
+            <nuxt-link to="/business1/B3Offshoe">
               <button class="btn btn-primary">More info</button>
             </nuxt-link>
           </div>
@@ -93,7 +93,7 @@
           <h2 class="text-black card-title">Asia foray consultants</h2>
           <p class="text-black">	Subsidiary established in Thailand, based on the business experience in China, we will help with Preparing to various procedures and site selection and offices opened.</p>
           <div class="card-actions">
-            <nuxt-link to="/B4Consult">
+            <nuxt-link to="/business1/B4Consult">
               <button class="btn btn-primary">More info</button>
             </nuxt-link>
           </div>
@@ -110,7 +110,7 @@
           <p class="text-black">	“Japanese→Other languages”“Other languages→Japanese”Both it can be translated.
 Japanese fluent Chinese and Thai staff have enrolled,It delivered in Comfortable quality.</p>
           <div class="card-actions">
-            <nuxt-link to="/B5Trans">
+            <nuxt-link to="/business1/B5Trans">
               <button class="btn btn-primary">More info</button>
             </nuxt-link>
           </div>
@@ -127,7 +127,7 @@ Japanese fluent Chinese and Thai staff have enrolled,It delivered in Comfortable
           <p class="text-black">Products that are not sold only in Thailand.It is a service to the import agency with their introduction.
 </p>
           <div class="card-actions">
-            <nuxt-link to="/B6Import">
+            <nuxt-link to="/business1/B6Import">
               <button class="btn btn-primary">More info</button>
             </nuxt-link>
           </div>
