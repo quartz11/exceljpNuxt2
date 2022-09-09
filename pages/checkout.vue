f9ba84ab300ee9543256e06f8597c505ce6f89dd
Quart11 <sitthichot.01sp@gmail.com>
2022-09-09 17:47:33 +0700

    Edit Checkout

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -189,7 +189,7 @@
                   <label for="middle_name">Middle Name</label>
                   <input v-model="middle_name" id="middle_name"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-8	 p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required>
+                    >
                   <br />
                   <label for="family_name">Family Name</label>
                   <input v-model="family_name" id="family_name"
